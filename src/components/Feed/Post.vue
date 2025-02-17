f4ab7c9c38e9336c093d02a26bff7880f9a1bb5f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-02-17 00:24:43 -0300

    feat: enhance post component for better usability and accessibility
    - Wrap post in an anchor tag to enable opening articles in a new tab
    - Use `<picture>` tag for improved image handling and fallback support
    - Add `bg-gray-800` to ensure a consistent background for images
    - Maintain semantic HTML structure for better accessibility

--- src/components/Feed/Post.vue
+++ src/components/Feed/Post.vue
@@ -1,12 +1,14 @@
 <template>
-	<div class="feed__post w-full h-screen relative cursor-pointer">
-		<img :src="post.image" :alt="post.title" class="w-full h-full object-cover absolute" />
+	<a target="_blank" :href="post.url" class="feed__post w-full h-screen relative cursor-pointer block">
+		<picture class="w-full h-full absolute bg-gray-800 block">
+			<img :src="post.image" :alt="post.title" class="w-full h-full object-cover" />
+		</picture>
 
 		<div class="absolute inset-x-0 bottom-0 p-4 pb-8 post-gradient">
 			<h1 class="text-white font-bold text-m lg:text-xl mb-1">{{ post.title }}</h1>
 			<p class="text-white text-xs lg:text-m">{{ post.content }}</p>
 		</div>
-	</div>
+	</a>
 </template>
 
 <script setup>
