9980fcfc8eb6bb66fcb25a26516ba170cec3d461
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-02-17 13:33:20 -0300

    feat: enhance feed component with category support Add support for fetching articles by category

--- src/components/Feed/Feed.vue
+++ src/components/Feed/Feed.vue
@@ -7,23 +7,20 @@
 <script setup>
 import { reactive, onMounted, onUnmounted, inject, watch } from "vue";
 import Post from "./Post.vue";
-import { fetchRandom, fetchPopular } from "@/utils/api";
+import { fetchRandom, fetchPopular, fetchCategory } from "@/utils/api";
+import { getCategoryEncoded } from "@/utils/categories"
 import LSKeys from "@/utils/localStorageKeys";
 
 const posts = reactive([]);
 const state = inject('langState');
+const initialTotal = 5;
 const defaultTotal = 10;
 let loading = false
 
-function handleScroll() {
-	if (
-		window.innerHeight + window.scrollY >=
-		document.body.offsetHeight - window.innerHeight * 3 && !loading
-	) {
-		loading = true;
-		populate(fetchRandom, state.currentLang, defaultTotal);
-	}
-}
+watch(() => state.currentLang, (newLang) => {
+	posts.length = 0;
+	populate(fetchRandom, newLang, defaultTotal);
+}, { immediate: true });
 
 async function populate(action, lang, total) {
 	const data = await action(lang, total);
@@ -34,10 +31,16 @@ async function populate(action, lang, total) {
 	loading = false;
 }
 
-watch(() => state.currentLang, (newLang) => {
-	posts.length = 0;
-	populate(fetchRandom, newLang, defaultTotal);
-}, { immediate: true });
+function handleScroll() {
+	if (
+		window.innerHeight + window.scrollY >=
+		document.body.offsetHeight - window.innerHeight * 3 &&
+		!loading
+	) {
+		loading = true;
+		populate(fetchRandom, state.currentLang, defaultTotal);
+	}
+}
 
 onMounted(async () => {
 	window.addEventListener("scroll", handleScroll);
@@ -56,9 +59,35 @@ onMounted(async () => {
 		}
 	}
 
-	populate(fetchFunction, state.currentLang, defaultTotal);
-
+	populate(fetchFunction, state.currentLang, initialTotal);
 	localStorage.setItem(LSKeys.feedInfo, today.toISOString());
+
+	const categories = Array.from({ length: 5 }).map(() => getCategoryEncoded());
+	console.log(categories);
+
+	function shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
+
+	const articles = [];
+
+	try {
+		const allArticles = await Promise.all(
+			categories.map(async (category) => {
+				const res = await fetchCategory(state.currentLang, defaultTotal, category);
+				return res;
+			})
+		);
+
+		articles.push(...allArticles.flat());
+		shuffleArray(articles);
+		articles.forEach((article) => posts.push(article));
+	} catch (error) {
+		console.error("Unable to load articles:", error);
+	}
 });
 
 onUnmounted(() => {
